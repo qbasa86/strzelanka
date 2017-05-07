2105a1ea3fbdd530a732f6d202315e4d2c2cc89e
qbasa <qbasa@wp.pl>
2017-05-07 10:57:14 +0200

    gierka

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1494146599,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/gracz-sheet0.png",
+		"images/potwor-sheet0.png",
+		"images/wybuch-sheet0.png",
+		"images/pocisk-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
